c6d9cbad52f7dc755e16fa20e75550b644c059ff
Humars89 <sgumar89@gmail.com>
2025-05-19 23:26:54 +0300

    Update Checking_test01.java

--- src/com/edu/task/test01/Checking_test01.java
+++ src/com/edu/task/test01/Checking_test01.java
@@ -1,7 +1,6 @@
 package com.edu.task.test01;
 
 public class Checking_test01 {
-
 	public static void main(String[] args) {
 		// Исходные данные
 		String[] dates = { "2024-04-24", "2024-04-25", "2024-04-26", "2024-04-27", "2024-04-28", "2024-04-29",
@@ -11,74 +10,43 @@ public class Checking_test01 {
 		int[] eveningTemps = { 12, 13, 14, 15, 17, 19, 18 };
 
 		// Вычисляем средние температуры
-		double[] avgTemps = calculateAverageTemperatures(morningTemps, dayTemps, eveningTemps);
-
-		// Определяем самый длинный период роста
-		int[] longestPeriod = findLongestIncreasingPeriod(avgTemps);
-
-		// Выводим результаты
-		printAverageTemperatures(dates, avgTemps);
-		printLongestIncreasingPeriod(dates, longestPeriod);
-	}
-
-	// Метод вычисления средней температуры
-	private static double[] calculateAverageTemperatures(int[] morning, int[] day, int[] evening) {
-		int length = morning.length;
-		double[] avgTemps = new double[length];
-
-		for (int i = 0; i < length; i++) {
-			avgTemps[i] = (morning[i] + day[i] + evening[i]) / 3.0;
+		double[] avgTemps = new double[dates.length];
+		for (int i = 0; i < dates.length; i++) {
+			avgTemps[i] = (morningTemps[i] + dayTemps[i] + eveningTemps[i]) / 3.0;
 		}
-		return avgTemps;
-	}
 
-	// Метод поиска самого длинного периода роста температуры
-	private static int[] findLongestIncreasingPeriod(double[] avgTemps) {
-		int maxStartIndex = -1, maxLength = 0;
-		int currentStartIndex = 0, currentLength = 1;
+		// Поиск самого длинного периода роста температуры
+		int maxStart = 0, maxLength = 0;
+		int currentStart = 0, currentLength = 1;
 
 		for (int i = 1; i < avgTemps.length; i++) {
 			if (avgTemps[i] > avgTemps[i - 1]) {
 				currentLength++;
-			} else {
 				if (currentLength > maxLength) {
-					maxStartIndex = currentStartIndex;
+					maxStart = currentStart;
 					maxLength = currentLength;
 				}
-				currentStartIndex = i;
+			} else {
+				currentStart = i;
 				currentLength = 1;
 			}
 		}
 
-		// Проверка последней последовательности
-		if (currentLength > maxLength) {
-			maxStartIndex = currentStartIndex;
-			maxLength = currentLength;
-		}
-
-		return new int[] { maxStartIndex, maxLength };
-	}
-
-	// Метод вывода средних температур
-	private static void printAverageTemperatures(String[] dates, double[] avgTemps) {
-		System.out.println("Средние температуры:");
+		// Вывод средних температур
+		System.out.println("Средние температуры за каждый день:");
 		for (int i = 0; i < dates.length; i++) {
-			System.out.printf("%s → %.1f\n", dates[i], avgTemps[i]);
+			System.out.println(dates[i] + ": " + (Math.round(avgTemps[i] * 10) / 10.0) + "C");
 		}
-	}
 
-	// Метод вывода информации о самом длинном периоде роста температуры
-	private static void printLongestIncreasingPeriod(String[] dates, int[] period) {
-		int startIndex = period[0];
-		int length = period[1];
-
-		if (length > 1) {
-			System.out.println("\nСамый длинный период повышения:");
-			System.out.printf("Дата начала: %s\n", dates[startIndex]);
-			System.out.printf("Дата конца: %s\n", dates[startIndex + length - 1]);
-			System.out.printf("Длина периода: %d дней\n", length);
+		// Вывод результата анализа
+		System.out.println("\nАнализ периода роста температур:");
+		if (maxLength > 1) {
+			System.out.println("Самый длинный период роста:");
+			System.out.println("Начало: " + dates[maxStart]);
+			System.out.println("Конец: " + dates[maxStart + maxLength - 1]);
+			System.out.println("Длительность: " + maxLength + " дней");
 		} else {
-			System.out.println("\nНет периода непрерывного повышения.");
+			System.out.println("Периодов роста температуры не обнаружено.");
 		}
 	}
 }
